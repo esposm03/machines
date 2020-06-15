ca8ac665bbc23d17008422793baeaa91d7f9c10c
esposm03 <36164633+esposm03@users.noreply.github.com>
2020-06-15 10:32:44 +0200

    Fix prompt and nix path

--- user-conf/setups/terminal.nix
+++ user-conf/setups/terminal.nix
@@ -7,10 +7,17 @@
 		exa
 	];
 
-    home.file.".config/ion/initrc".text = ''
-		alias ls = "exa"
-		alias ll = "exa --long --header --group-directories-first"
+    home.file.".config/ion/initrc".text =
 
-		eval $(starship init ion)
-	'';
+    ''
+    alias ls = "exa"
+    alias ll = "exa --long --header --group-directories-first"
+
+    fn PROMPT
+        starship prompt --status $? --jobs $(jobs ^| wc -l)
+    end
+
+    export NIX_PATH="~/.nix-defexpr/channels:$NIX_PATH"
+    '';
+
 }
