a09fa5f96c55a603527fe326bdb4ca9e4c040bad
esposm03 <36164633+esposm03@users.noreply.github.com>
2020-08-22 16:41:08 +0000

    Remove gnome desktop environment

--- system-conf/configuration.nix
+++ system-conf/configuration.nix
@@ -27,9 +27,6 @@
 	];
 
 	programs.sway.enable = true;
-	services.xserver.enable = true;
-	services.xserver.desktopManager.gnome3.enable = true;
-	services.xserver.displayManager.gdm.enable = true;
 
 
 
